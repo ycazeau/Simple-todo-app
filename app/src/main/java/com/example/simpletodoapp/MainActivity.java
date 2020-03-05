5daf33f751fc40d968b48a5cac157b824e906b60
ycazeau <ycazeau23@gmail.com>
2020-03-05 09:54:09 -0500

    Adding items to the List

--- app/src/main/java/com/example/simpletodoapp/MainActivity.java
+++ app/src/main/java/com/example/simpletodoapp/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +36,22 @@ public class MainActivity extends AppCompatActivity {
         items.add("Read Bible");
         items.add("Doing exercices");
         items.add("Eat rice and vegetables"  );
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        final ItemsAdapter itemsAdapter = new ItemsAdapter(items);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem =etItem.getText().toString();
+                //Add Item to model
+                items.add(todoItem);
+                //Notify adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size()-1);
+                etItem.setText("");
+                Toast.makeText(getApplicationContext(),"Item was added", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }

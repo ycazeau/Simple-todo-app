bd295f359162ce4cc2782f23a8ccdf5a31f9c43c
ycazeau <ycazeau23@gmail.com>
2020-03-05 22:57:17 -0500

    clear file

--- app/src/main/java/com/example/simpletodoapp/MainActivity.java
+++ app/src/main/java/com/example/simpletodoapp/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.FileUtils;
+import org.apache.commons.io.FileUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +23,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public static final String KEY_ITEM_TEXT = "item_text";
-    public static final String KEY_ITEM_POSITION = "item_text";
+    public static final String KEY_ITEM_POSITION = "item_position";
     public static final int EDIT_TEXT_MODE = 20;
 
     List<String> items;
@@ -44,15 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         loadItems();
 
-        ItemsAdapter.OnClickListener onClickListener = new ItemsAdapter.OnClickListener() {
-            @Override
-            public void onItemClicked(int position) {
-                Intent i = new Intent(MainActivity.this,EditActivity.class);
-                i.putExtra(KEY_ITEM_TEXT,items.get(position));
-                i.putExtra(KEY_ITEM_POSITION, position);
-                startActivityForResult(i,EDIT_TEXT_MODE);
-            }
-        };
+
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
             @Override
             public void onItemLongClicked(int position) {
@@ -62,6 +54,16 @@ public class MainActivity extends AppCompatActivity {
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(),"Item was removed",Toast.LENGTH_SHORT).show();
                 saveItems();
+            }
+        };
+
+        ItemsAdapter.OnClickListener onClickListener = new ItemsAdapter.OnClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                Intent i = new Intent(MainActivity.this,EditActivity.class);
+                i.putExtra(KEY_ITEM_TEXT,items.get(position));
+                i.putExtra(KEY_ITEM_POSITION, position);
+                startActivityForResult(i,EDIT_TEXT_MODE);
             }
         };
 
@@ -87,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK && requestCode ==EDIT_TEXT_MODE){
+        if (resultCode == RESULT_OK && requestCode == EDIT_TEXT_MODE){
             // Retreive the update text value
             String itemText = data.getStringExtra(KEY_ITEM_TEXT);
             // Extract the original position of the edited intem from the position Key
@@ -105,13 +107,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private File getDataFile(){
-        return new File(getFilesDir(), "data.txt");
+        return new File(getFilesDir(), "datas.txt");
     }
 
     // This function will load items by reading every line of the data file
     private void loadItems(){
        try{
-           items = new ArrayList<>(org.apache.commons.io.FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+           items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
        } catch (IOException e){
            Log.e("MainActivity", "Error reading items",e);
            items = new ArrayList<>();
@@ -120,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
     //This function saves items by writing them into the data
     private void saveItems(){
         try{
-            org.apache.commons.io.FileUtils.writeLines(getDataFile(), items);
+            FileUtils.writeLines(getDataFile(), items);
         } catch (IOException e){
             Log.e("MainActivity", "Error writing items",e);
             items = new ArrayList<>();
